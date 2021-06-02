19e7ff4b4cb058a5544380cd8fba03d52b16a38d
SAM Neo <talk2me.sam@gmail.com>
2021-06-02 17:46:38 +0100

    Update init-node.sh

--- init-node.sh
+++ init-node.sh
@@ -5,11 +5,9 @@
 sudo apt -y update
 sudo apt -y upgrade
 #
-# Dependency - Install Java
+# Install the required tools:
+## Java
 sudo apt install -y default-jre
-#
-# Install Ansible
-sudo apt -y install ansible
 #
 # Remove unecessary apps
 sudo apt -y autoremove
